b6008d3aa145050f557fb237fbbe1ae1fb56b93b
Nick <nberra90@gmail.com>
2019-01-14 09:55:16 -0600

    edit text

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -30,11 +30,22 @@ export class TodoItem extends React.Component <TodoItemProps, TodoItemState> {
       id: this.props.id,
       deleted: false
     }
-    
+    this.editTodo = this.editTodo.bind(this);
   }
 
   toggleCompleted() {
     this.setState({ completed: !this.state.completed })
+  }
+
+  editTodo(event: React.SyntheticEvent<{name: string, value: string}>) {
+
+    let change = {
+      title: this.state.title,
+      summary: this.state.summary
+    };
+
+    // change[key] = event.target.value;
+    this.setState({...this.state, [event.currentTarget.name]: event.currentTarget.value});
   }
 
   render() {
@@ -44,6 +55,7 @@ export class TodoItem extends React.Component <TodoItemProps, TodoItemState> {
         <h2>{this.state.title}</h2>
         <p>{this.state.summary}</p>
         <input type="checkbox" checked={this.state.completed} onChange={this.toggleCompleted}/>
+        <input type="text" name="title" value={this.state.title} onChange={this.editTodo}/><textarea name="summary" value={this.state.summary} onChange={this.editTodo}></textarea>
         <button onClick={this.props.remove}>Delete</button>
       </li>
     );
